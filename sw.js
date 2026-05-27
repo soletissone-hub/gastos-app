2ce2de80672e5ac81255961bbd13ad35d8225f30
soletissone-hub <soledad.tissone@utel.edu.mx>
2026-05-27 20:55:27 -0300

    Bump cache a v4 para invalidar index.html viejo en service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v3';
+const CACHE = 'gastos-v4';
 const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/classifier.js', '/api.js'];
 
 self.addEventListener('install', e => {

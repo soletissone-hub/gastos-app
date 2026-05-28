25d4986dc27b35ad105d613eeb81546592274cc2
soletissone-hub <soledad.tissone@utel.edu.mx>
2026-05-28 15:21:47 -0300

    Historico: linea de total siempre visible en todos los filtros

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v8';
+const CACHE = 'gastos-v9';
 const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/classifier.js', '/api.js'];
 
 self.addEventListener('install', e => {

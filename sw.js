60a48368c76189edb0bf259c365487c5c9ca6780
soletissone-hub <soledad.tissone@utel.edu.mx>
2026-05-28 15:33:32 -0300

    Agregar tab Movimientos: filtro por medio/persona/periodo, orden mas reciente

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v10';
+const CACHE = 'gastos-v11';
 const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/classifier.js', '/api.js'];
 
 self.addEventListener('install', e => {

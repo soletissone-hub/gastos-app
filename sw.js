0d51148d55e33202cec23e8173eb98ec394771d5
soletissone-hub <soledad.tissone@utel.edu.mx>
2026-05-29 15:57:38 -0300

    Histórico: filtro por medio de pago + totales y variación % en gráfico

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v18';
+const CACHE = 'gastos-v19';
 const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/classifier.js', '/api.js'];
 
 self.addEventListener('install', e => {
